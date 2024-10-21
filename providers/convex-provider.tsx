d90e7829894ee2ee9f6870fa4e3a11f3a55a9838
IBEABUCHI MIRACLE <chikimiracle2000@gmail.com>
2024-10-22 00:15:57 +0100

    add shadcn components + add tanstack-query

--- providers/convex-provider.tsx
+++ providers/convex-provider.tsx
@@ -1,16 +1,29 @@
 "use client";
 
 import { ReactNode } from "react";
+import { ConvexQueryClient } from "@convex-dev/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ConvexReactClient } from "convex/react";
 
 import { ConvexAuthNextjsProvider } from "@convex-dev/auth/nextjs";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
+const convexQueryClient = new ConvexQueryClient(convex);
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      queryKeyHashFn: convexQueryClient.hashFn(),
+      queryFn: convexQueryClient.queryFn(),
+    },
+  },
+});
+convexQueryClient.connect(queryClient);
+
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
   return (
     <ConvexAuthNextjsProvider client={convex}>
-      {children}
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </ConvexAuthNextjsProvider>
   );
 }
